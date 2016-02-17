85da9fba993e861a6de3eec4825c300a8f0f2e36
Sam Myers <sam@students.olin.edu>
2016-02-17 09:22:09 -0500

    added skeleton code

--- boats.py
+++ boats.py
@@ -1,2 +1,12 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from stl import mesh
+import scipy.integrate as integrate
+import sympy
+
+x = np.linspace(-1, 1, 1000)
+# y = np.abs(x)**3 - 1
+y = lambda x: np.abs(x)**3 - 1
+plt.plot(x, y(x))
+plt.show()
+print abs(integrate.quad(y, -1, 1)[0])
