936dafeed1664c53657d42cb319ea0d8174839b1
Sam Myers <sam@students.olin.edu>
2016-02-17 12:30:25 -0500

    still sketching

--- boats.py
+++ boats.py
@@ -12,11 +12,17 @@ ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z')
 
+ax.set_xlim(-1.85, 1.85)
+ax.set_ylim(-5, 5)
+ax.set_zlim(-2.1, 0)
+
 x = np.linspace(-1.85, 0, 1000)
 x2 = np.linspace(0, 1.85, 1000)
+
 y = lambda x: np.tan(-(x+0.95)**3) + (x+0.95)**2 - 1.8
-y2 = lambda x: np.tan((x+0.9)**3) + (-(x+0.9))**2 - 1.8
-z = lambda x: np.exp(0.1*x**2)
+y2 = lambda x: np.tan((x-0.95)**3) + (-(x-0.95))**2 - 1.8
+
+z = lambda x: -np.log(x+1) - 5
 
 # X, Z = np.meshgrid(x, y(x))
 # Y = z(X)
@@ -27,13 +33,14 @@ z = lambda x: np.exp(0.1*x**2)
 
 # xy = ax.plot(x, )
 xz = ax.plot(x, np.linspace(0, 0, 1000), y(x))
-xz2 = ax.plot(x2, np.linspace(0, 0, 1000), y2(x))
+xz2 = ax.plot(x2, np.linspace(0, 0, 1000), y2(x2))
+
+xy = ax.plot(x, z(x), np.linspace(y2(1.85), y2(1.85), 1000))
 
 # print abs(integrate.quad(y, -1, 1)[0])
 print y(0)
 print y(-1.85)
 
-plt.plot(x, z(x))
 plt.show()
 
 weighted_mass = lambda x, y: np.array([])
